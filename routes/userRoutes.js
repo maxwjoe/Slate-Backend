28b6f1c63e41b4fa5c69d0daa29315041465135f
Joel <joel.r.maxwell@gmail.com>
2022-09-01 12:03:25 +1000

    Manual CORS Configuration

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,16 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {registerUser, loginUser, getCurrUser, updateUser} = require('../controllers/userController');
 const {protect} = require("../middleware/authMiddleware")
-const cors = require('cors');
 
-const corsOptions ={
-    origin:'*', 
-    methods: ["PUT", "POST", "GET", "DELETE", "OPTIONS"],
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-
-router.use(cors(corsOptions));
 router.post('/', registerUser)
 router.post('/login', loginUser)
 router.get('/current', protect, getCurrUser)
--- server.js
+++ server.js
@@ -3,20 +3,31 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT;
 const {errorHandler} = require("./middleware/errorMiddleware")
 const connectDB = require('./config/db');
-const cors = require('cors');
 //
 // --- Setup ---
 connectDB();
 const app = express();
-const corsOptions ={
-    origin:'*', 
-    methods: ["PUT", "POST", "GET", "DELETE", "OPTIONS"],
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
 
 // --- Middleware ---
-app.use(cors(corsOptions));
+
+//Cors Configuration - Start
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
+  //Cors Configuration - End
+
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
